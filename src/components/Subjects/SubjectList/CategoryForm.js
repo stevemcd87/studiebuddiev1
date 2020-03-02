3dc4e91099b9b94eee4de74dc465ac4738b8ed3d
Stephen McDonald <stephenmcdonald8787@gmail.com>
2020-03-02 12:38:52 -0500

    ability to create category to subject, must do CRUD and orderBy created time

---
+++ src/components/Subjects/SubjectList/CategoryForm.js
@@ -0,0 +1,45 @@
+import React, { useState, useContext } from "react";
+import ApiContext from "../../../contexts/ApiContext";
+
+export default function CategoryForm(props) {
+  let { subject } = props,
+    { API, user } = useContext(ApiContext),
+    [categoryName, setCategoryName] = useState(""),
+    [categoryDesc, setCategoryDesc] = useState("");
+  return (
+    <form>
+      <input
+        type="text"
+        onChange={e => setCategoryName(e.target.value)}
+        defaultValue={categoryName}
+      />
+      <input
+        type="text"
+        onChange={e => setCategoryDesc(e.target.value)}
+        defaultValue={categoryDesc}
+      />
+      <button type="button" onClick={postCategory}>
+        Create
+      </button>
+    </form>
+  );
+  function postCategory() {
+    API.post("StuddieBuddie", `/subjects/${subject.pathName}`, {
+      body: JSON.stringify({
+        categoryName: categoryName.trim(),
+        categoryDesc: categoryDesc.trim(),
+        username: user.user.username
+        // pathName: subject.pathName
+      })
+    })
+      .then(response => {
+        console.log("response postCategory");
+        console.log(response);
+        // getSubjects();
+      })
+      .catch(error => {
+        console.log("err postCategory");
+        console.log(error.response);
+      });
+  }
+}
--- src/components/Subjects/SubjectList/SubjectDetail.js
+++ src/components/Subjects/SubjectList/SubjectDetail.js
@@ -2,39 +2,67 @@ import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import ApiContext from "../../../contexts/ApiContext";
 import SubjectContext from "../../../contexts/SubjectContext";
+import CategoryForm from "./CategoryForm";
+
 function SubjectDetail() {
   let { pathName } = useParams(),
     { API, user } = useContext(ApiContext),
-    [subject, setSubject] = useState();
+    [subject, setSubject] = useState({}),
+    [categories, setCategories] = useState([]),
+    [displayCategoryForm, setDisplayCategoryForm] = useState(false);
   // { subject } = useContext(SubjectContext);
   useEffect(() => {
     getSubject();
   }, []);
   return (
-    <div className="subject-detail">
-      {subject && (
-        <>
-          <p>Creator: {subject.username}</p>
-          <p>{subject.name}</p>
-          <p>{subject.desc}</p>
-          <button type="button" onClick={createCategory}>
-            Create Category
-          </button>
-        </>
-      )}
+    <div className="subject-detail-component">
+      <div className="subject-detail">
+        <p>Creator: {subject.username}</p>
+        <p>{subject.navName}</p>
+        <p>{subject.subjectDesc}</p>
+      </div>
+      <div>
+        {categories.map(category => {
+          return (
+            <div key={category.desc} className="category">
+              <p>
+                {category.name} - {category.desc}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+      <button
+        type="button"
+        onClick={() => setDisplayCategoryForm(!displayCategoryForm)}
+      >
+        Create Category
+      </button>
+      {displayCategoryForm && <CategoryForm {...{ subject }} />}
     </div>
   );
   function getSubject() {
     console.log("GET subject");
     API.get("StuddieBuddie", `/subjects/${pathName}`, {
-      body: {
+      queryStringParameters: {
         username: user.user.username
       }
     })
       .then(response => {
-        console.log("res");
+        console.log("res getSubject");
         console.log(response);
         setSubject(response[0]);
+        let c = response.slice(1).map(v => {
+          return {
+            name: v.pathName
+              .split("#")[1]
+              .split("_")[0]
+              .replace("-", " "),
+            desc: v.categoryDesc
+          };
+        });
+        console.log(c);
+        setCategories(c);
         // return response.data[0];
       })
       .catch(error => {
